672f5f1e7a8045da89e9b62a7c1eeefabf3e6551
Evan <evjava@gmail.com>
2022-09-04 13:41:07 -0400

    added 2p font, converted stage timer to human readable form

--- Assets/Scripts/Game Stage 1/stageManager.cs
+++ Assets/Scripts/Game Stage 1/stageManager.cs
@@ -7,11 +7,21 @@ public class stageManager : MonoBehaviour
 {
     public float currentTime;
     public TMPro.TMP_Text timerText;
-
+    private string currentTimeString;
     private void stageTimer()
     {
         currentTime = Time.timeSinceLevelLoad;
-        timerText.text = currentTime.ToString();
+        int minutes = (int)currentTime / 60;
+        int seconds = 0;
+        if(currentTime < 60) {
+            seconds = (int)currentTime;
+        }
+        if(currentTime > 60)
+        {
+            seconds = (int)currentTime - (60 * minutes);
+        }
+        currentTimeString =  string.Format("{0:00}:{1:00}", minutes, seconds);
+        timerText.text = currentTimeString;
     }
 
 
