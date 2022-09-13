84baa4376bffcfb1a0972c4f4041e8d2104e6057
Jordan Davidson <jdoubled05@gmail.com>
2022-09-12 21:42:34 -0400

    added tilemap, player, movement control

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    public float collisionOffset = 0.05f;
+    public ContactFilter2D movementFilter;
+
+    Vector2 movementInput;
+    Rigidbody2D rb;
+    SpriteRenderer spriteRenderer;
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (movementInput != Vector2.zero)
+        {
+            bool success = TryMove(movementInput);
+
+            if (!success)
+            {
+                success = TryMove(new Vector2(movementInput.x, 0));
+
+                if (!success)
+                {
+                    success = TryMove(new Vector2(0, movementInput.y));
+                }
+            }
+        }
+
+        if (movementInput.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (movementInput.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+
+    private bool TryMove(Vector2 direction)
+    {
+        // Check for collisons
+        int count = rb.Cast(
+                    direction, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
+                    movementFilter, // The settings that determine where a collision can occur on such as layers to collide with
+                    castCollisions, // List of collisions to store the found collisions into after the Cast is finished
+                    moveSpeed * Time.fixedDeltaTime + collisionOffset // The amount to cast equal to the movement plus an offset
+                );
+
+        if (count == 0)
+        {
+            rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void OnMove(InputValue movemventValue)
+    {
+        movementInput = movemventValue.Get<Vector2>();
+    }
+}
